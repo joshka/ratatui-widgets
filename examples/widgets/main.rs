1c8aae0738947888e99f5f01863ff5dc549843b5
Josh McKinney <joshka@users.noreply.github.com>
2024-10-02 16:52:05 -0700

    chore: bump ratatui to 0.28.1 and related deps

--- examples/widgets/main.rs
+++ examples/widgets/main.rs
@@ -1,8 +1,11 @@
 use std::io::stdout;
 
-use color_eyre::{config::HookBuilder, Result};
-use crossterm::{event::*, terminal::*, ExecutableCommand};
-use ratatui::prelude::*;
+use color_eyre::Result;
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    ExecutableCommand,
+};
+use ratatui::DefaultTerminal;
 
 mod app;
 mod tabs {
@@ -14,41 +17,16 @@ mod tabs {
 use app::App;
 
 fn main() -> Result<()> {
-    init_error_hooks()?;
-    let terminal = init_terminal()?;
-    App::new().run(terminal)?;
-    let _ = restore_terminal();
-    Ok(())
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
 }
 
-/// install panic and error hooks that restore the terminal before displaying the error messages
-fn init_error_hooks() -> Result<()> {
-    let (panic, error) = HookBuilder::default().into_hooks();
-    let panic = panic.into_panic_hook();
-    let error = error.into_eyre_hook();
-    color_eyre::eyre::set_hook(Box::new(move |e| {
-        let _ = restore_terminal();
-        error(e)
-    }))?;
-    std::panic::set_hook(Box::new(move |info| {
-        let _ = restore_terminal();
-        panic(info)
-    }));
-    Ok(())
-}
-
-fn init_terminal() -> Result<Terminal<impl Backend>> {
-    enable_raw_mode()?;
-    stdout().execute(EnterAlternateScreen)?;
+fn run(terminal: DefaultTerminal) -> Result<()> {
     stdout().execute(EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout());
-    let terminal = Terminal::new(backend)?;
-    Ok(terminal)
-}
-
-fn restore_terminal() -> Result<()> {
-    disable_raw_mode()?;
-    stdout().execute(LeaveAlternateScreen)?;
+    App::new().run(terminal)?;
     stdout().execute(DisableMouseCapture)?;
     Ok(())
 }
